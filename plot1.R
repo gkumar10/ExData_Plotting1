c54dcd63c2529ad15c3034ef235a1e594ce22a93
gkumar10 <gkumar10@users.noreply.github.com>
2014-10-07 12:12:58 -0700

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,5 +1,5 @@
-pc <- read.table("~/Exploratory-Data-Analysis/household_power_consumption.txt", header=FALSE, sep=";", nrows=2880, skip=66637, na.strings="?")
+pc <- read.table("~/household_power_consumption.txt", header=FALSE, sep=";", nrows=2880, skip=66637, na.strings="?")
 names(pc) <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
-png(file="~/Coursera/Exploratory-Data-Analysis/plot1.png", width=480, height=480)
+png(file="~/plot1.png", width=480, height=480)
 hist(pc$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
 dev.off()
